9ecffba94fb064dac5b516d611d538c9de67b078
Zdravko Zdravkov <zdr0@yahoo.com>
2020-12-17 10:10:46 +0100

    Best resource in constellation

---
+++ Api/EveEchoesPlanetaryProductionApi.Api.Models/PlanetaryResources/BestPlanetaryResourcesInConstellation/BestPlanetaryResourcesInConstellationModel.cs
@@ -0,0 +1,13 @@
+﻿namespace EveEchoesPlanetaryProductionApi.Api.Models.PlanetaryResources.BestPlanetaryResourcesInConstellation
+{
+    using System.Collections.Generic;
+
+    using EveEchoesPlanetaryProductionApi.Services.Data.Models;
+
+    public class BestPlanetaryResourcesInConstellationModel
+    {
+        public int Count { get; set; }
+
+        public IEnumerable<BestResourceServiceModel> Resources { get; set; }
+    }
+}
